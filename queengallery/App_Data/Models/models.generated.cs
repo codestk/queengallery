27f3ac5413dd15c7d8caf0a6759fcc6206c6c39e
momojojo <momojojo@HOMEPC>
2016-11-03 20:56:02 +0700

    ytrytrytr

--- queengallery/App_Data/Models/models.generated.cs
+++ queengallery/App_Data/Models/models.generated.cs
@@ -19,8 +19,8 @@ using Umbraco.ModelsBuilder;
 using Umbraco.ModelsBuilder.Umbraco;
 
 [assembly: PureLiveAssembly]
-[assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "2fe867e1b09289bc")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.9")]
+[assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "c01e0cf485e6438d")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 namespace Umbraco.Web.PublishedContentModels
 {
