c34ac27457d4d82066427d74e9d2329d318496ff
momojojo <momojojo@HOMEPC>
2016-12-08 21:12:25 +0700

    ffff

--- queengallery/Modules/ToTop/ToTop.js
+++ queengallery/Modules/ToTop/ToTop.js
@@ -1,7 +1,24 @@
- function ToTop()
- {
-var body = $("html, body");
-body.stop().animate({scrollTop:0}, '1', 'swing', function() { 
-  
-});
- }
+function ToTop() {
+    //$('body').animatescroll();
+    //var scrollPosition = document.body.scrollTop;
+    //console.log(scrollPosition);
+    //if (scrollPosition > 0) {
+    //    // top of the page
+    //    $('body').animatescroll({ scrollSpeed: 2000, easing: 'easeInOutBack' });
+    //}
+
+    var $window = $(window);
+    $topOffset = $(this).scrollTop();
+    if ($topOffset > 400) {
+        // top of the page
+        $('body').animatescroll({ scrollSpeed: 2000, easing: 'easeInOutBack' });
+    }
+}
+
+//For Check Point
+//var $window = $(window);
+
+//$(window).on('scroll', function() {
+//    $topOffset = $(this).scrollTop();
+
+//    console.log($topOffset);
