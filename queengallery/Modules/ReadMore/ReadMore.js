86266f0d5010d533efddfe45b78b7b629705740c
momojojo <momojojo@HOMEPC>
2016-12-14 17:42:42 +0700

    ffff

--- queengallery/Modules/ReadMore/ReadMore.js
+++ queengallery/Modules/ReadMore/ReadMore.js
@@ -3,13 +3,13 @@
     ellipsestext: "...",
     moretext: "Show more >",
     lesstext: "Show less",
-
+    showMore: true,
     BindEvent: function () {
         var _showChar = this.showChar;
         var _ellipsestext = this.ellipsestext;
         var _moretext = this.moretext;
         var _lesstext = this.lesstext;
-       
+        var _showMore = this.showMore;
         $('.more').each(function () {
             var content = $(this).html();
             
@@ -24,6 +24,16 @@
  
                 //var html = c + '<span class="moreellipses">' + _ellipsestext + '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp <a href="" class="morelink">' + _moretext + '</a></span>';
                 var html = c + '<span class="moreellipses">' + _ellipsestext + '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp <a href="" class="morelink">' + _moretext + '</a></span>';
+                if (_showMore == true) {
+                    html = c + '<span class="moreellipses">' + _ellipsestext + '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp <a href="" class="morelink">' + _moretext + '</a></span>';
+                } else {
+
+                    html = c + '<span class="moreellipses">' + _ellipsestext + '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp <a href="" class="morelink"></a></span>';
+                }
+
+
+
+
                 $(this).html(html);
             }
         });
