eaba0d89a7f877952c16edda12de504ea9b625e3
momojojo <momojojo@HOMEPC>
2017-01-11 14:43:37 +0700

    PPPPP

--- queengallery/App_Data/Models/models.generated.cs
+++ queengallery/App_Data/Models/models.generated.cs
@@ -20,7 +20,7 @@ using Umbraco.ModelsBuilder.Umbraco;
 
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "805f2a072d34b259")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.3")]
 
 namespace Umbraco.Web.PublishedContentModels
 {
